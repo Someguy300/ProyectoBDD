dfa4c225e9d822768cfd43408cb5672a9a0c4266
gbanezca <gbanezca@gmail.com>
2020-05-30 19:43:25 -0400

    intento 2

--- backproy/backproy/urls.py
+++ backproy/backproy/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rest_framework import routers 
-from api import views 
+
+
 
 
 router = routers.DefaultRouter()
