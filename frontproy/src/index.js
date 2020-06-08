20c27af4dc28c9000bda60c6be2ff2525d32bcbe
Emilianolight <emiliano.mujica@correo.unimet.edu.ve>
2020-06-08 11:05:15 -0400

    Front1

--- frontproy/src/index.js
+++ frontproy/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
