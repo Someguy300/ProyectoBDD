5c7b277f8f1f9c8cbf9d019fa25db097e1613660
Emilianolight <emiliano.mujica@correo.unimet.edu.ve>
2020-06-26 10:25:33 -0400

    Cambios Home

--- frontproy/src/Components/Footer.js
+++ frontproy/src/Components/Footer.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import '../Components/Footer.css';
+
 
 function Footer() {
   return (
    
-<footer className="page-footer font-small ">
-
-  <div className="footer-copyright text-center py-3">© 2020 Copyright:
-    <a href> Metrocine</a>
-  </div>
-
-</footer>
+   
+   
+    
+     
+      <div class="footer-copyright text-center py-3">© 2020 Copyright:
+        <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+      </div>
+     
+    
+   
     
   );
 }
--- frontproy/src/Components/Home.js
+++ frontproy/src/Components/Home.js
@@ -1,33 +1,29 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
-import '../Components/Home.css';
+import './Home.css';
 function Home() {
   return (
     <div>
    <Navbar></Navbar>
 
-    <body>
-      <h1 className="Title "> Pròximos Estrenos</h1>
-    <div className="slider ">
-              <ul>
+   <div className="slider">
+        <h1 className="title">Estrenos</h1>
+          <ul>
+               <li> <img src="../Avengers.png"  width="60%" height="80%"   /> </li>
 
-                  <li>  <img src="Avengers.png"    width="700px"  height="500px"   /></li>
-                  <li>  <img src="Conjuro.png"      width="700px"  height="500px"       /></li>
-                  <li>  <img src="deadpool.png"       width="700px"  height="500px"      /></li>
-                  <li>  <img src="frozen.png"     width="700px"  height="500px"        /></li>       
-                           
-              </ul>
-    </div>
-    <h1 className="Title"> Sedes</h1>
-    <div></div>
-    </body>
+               <li><img src="../deadpool.png" width="60%" height="80%"    /></li>
 
+               <li> <img src="../frozen.png"   width="60%" height="80%"   /></li>
 
-
-  <Footer></Footer>
-  </div>
+               <li><img src="../Conjuro.png"   width="60%" height="80%"   /></li>
+           </ul>
   
+    </div>
+    <hr className="linea"></hr>
+  <Footer></Footer>
+  
+  </div>
   );
 }
 
