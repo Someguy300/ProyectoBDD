1c6fae1ac9a5bd286c0f99619730c3a8751855e5
Someguy300 <Someguy300@users.noreply.github.com>
2020-06-29 16:56:35 -0400

    pruebas conexion backend

--- backproy/api/serializers.py
+++ backproy/api/serializers.py
@@ -1,52 +1,52 @@
 from rest_framework import serializers,fields
 from .models import *
 
-class PeliculaSerializer(serializers.HyperlinkedModelSerializer):
+class PeliculaSerializer(serializers.ModelSerializer):
     class Meta: 
         model = Pelicula 
         fields = ('__all__') 
     genero = fields.MultipleChoiceField(choices=GENEROS,style={'base_template':'checkbox_multiple.html'})
     lenguaje = fields.MultipleChoiceField(choices=LENGUAJES,style={'base_template':'checkbox_multiple.html'})
 
-class ClienteSerializer(serializers.HyperlinkedModelSerializer):
+class ClienteSerializer(serializers.ModelSerializer):
     
     class Meta: 
         model = Cliente 
         fields = ('__all__') 
 
-class EntradaSerializer(serializers.HyperlinkedModelSerializer):
+class EntradaSerializer(serializers.ModelSerializer):
 
     class Meta: 
         model = Entrada 
         fields = ('__all__') 
 
-class FacturaSerializer(serializers.HyperlinkedModelSerializer):
+class FacturaSerializer(serializers.ModelSerializer):
 
     class Meta: 
         model = Factura 
         fields = ('__all__') 
     
 
-class ProductoSerializer(serializers.HyperlinkedModelSerializer):
+class ProductoSerializer(serializers.ModelSerializer):
 
     class Meta: 
         model = Producto 
         fields = ('__all__') 
 
-class FuncionSerializer(serializers.HyperlinkedModelSerializer):
+class FuncionSerializer(serializers.ModelSerializer):
 
     class Meta: 
         model = Funcion 
         fields = ('__all__') 
 
 
-class SalaSerializer(serializers.HyperlinkedModelSerializer):
+class SalaSerializer(serializers.ModelSerializer):
 
     class Meta: 
         model = Sala 
         fields = ('__all__') 
 
-class SedeSerializer(serializers.HyperlinkedModelSerializer):
+class SedeSerializer(serializers.ModelSerializer):
 
     class Meta: 
         model = Sede 
