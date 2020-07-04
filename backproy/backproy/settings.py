85c5f73c534638678e9a394b5e8cc77732ef5309
gbanezca <gbanezca@gmail.com>
2020-07-04 15:40:26 -0400

    venv y axios

--- backproy/backproy/settings.py
+++ backproy/backproy/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'backproy',
-        'USER': 'backadmin',
+        'USER': 'root',
         'PASSWORD': '12345',
         'HOST': 'localhost',
         'PORT': '3306',
